6de904c51ffe0a8b1fea56a55a24642886993a26
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2023-09-09 01:22:49 +0530

    feat: application completed

---
+++ Frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:8000/book/delete/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book deleted Successfully", { variant: "error" });
+        navigate("/");
+      })
+      .catch((err) => {
+        enqueueSnackbar("Error Deleting Book", { varient: "error" });
+        console.log(err);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">
+          Are you sure you want to delete this book..?
+        </h3>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteBook;
